ec9da932523154d322f7c01e0943d4cc607340ce
MyAeroCode <meteo_meteo@naver.com>
2020-10-11 21:49:46 +0900

    #4.1 Testing GET movies id

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,32 @@
 import { Test, TestingModule } from "@nestjs/testing";
-import { INestApplication } from "@nestjs/common";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
 import * as request from "supertest";
 import { AppModule } from "./../src/app.module";
 
 describe("AppController (e2e)", () => {
     let app: INestApplication;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [AppModule],
         }).compile();
 
         app = moduleFixture.createNestApplication();
+        app.useGlobalPipes(
+            new ValidationPipe({
+                //
+                // 데코레이터가 설정되어 있지 않은 프로퍼티는 제거됩니다.
+                whitelist: true,
+
+                //
+                // 데코레이터가 설정되어 있지 않은 프로퍼티가 전달되면 에러를 발생시킵니다.
+                forbidNonWhitelisted: true,
+
+                //
+                // url param을 실제 원하는 타입으로 변환합니다.
+                transform: true,
+            }),
+        );
         await app.init();
     });
 
@@ -48,5 +63,22 @@ describe("AppController (e2e)", () => {
                 .delete("/movies")
                 .expect(404);
         });
+    });
+
+    describe("/movies/:id", () => {
+        it("GET", () => {
+            return request(app.getHttpServer())
+                .get("/movies/1")
+                .expect(200);
+        });
+
+        it("GET 404", () => {
+            return request(app.getHttpServer())
+                .get("/movies/999")
+                .expect(404);
+        });
+
+        it.todo("DELETE");
+        it.todo("PATCH");
     });
 });
