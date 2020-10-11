f32be57116a1bf892bc969be52d48076a59945ef
MyAeroCode <meteo_meteo@naver.com>
2020-10-11 21:34:23 +0900

    add root path module, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { MoviesModule } from "./movies/movies.module";
+import { AppService } from "./app.service";
+import { AppController } from "./app.controller";
 
 @Module({
     imports: [MoviesModule],
-    controllers: [],
-    providers: [],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
